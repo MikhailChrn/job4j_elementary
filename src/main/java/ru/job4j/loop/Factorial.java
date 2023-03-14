1885a530a023ee66c683b90824328ea928fdd8c3
Mikhail <mikhail.cherneyev@yandex.ru>
2023-03-14 18:14:46 +0400

    5.2. Создать программу, вычисляющую факториал. [#193] Реализуйте метод вычисления факториала. В курсе используется только автоматическое тестирование за счет JUnit.

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result;
+        if (n == 0) {
+            result = 1;
+        } else {
+            result = 1;
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
