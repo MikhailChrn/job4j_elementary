cb28df80ef6bc0ef36d2b73b1e79be0349be8f13
Mikhail <mikhail.cherneyev@yandex.ru>
2023-03-24 11:28:41 +0400

    3. Перегрузить метод max для трех чисел [#122644] В классе ru.job4j.condition.Max из задания 4.2. Тернарное сравнение добавить перегруженный метод max для трех и четырех чисел. Постарайтесь, аналогично, примеру из задания переиспользовать уже существующие методы.

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,14 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int a1, int a2, int a3) {
+        int max = max(a1, a2);
+        return max > a3 ? max : a3;
+    }
+
+    public static int max(int b1, int b2, int b3, int b4) {
+        int max = max(b1, b2, b3);
+        return max > b4 ? max : b4;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -30,4 +30,25 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax2To2To3Then3() {
+        int a1 = 2;
+        int a2 = 2;
+        int a3 = 3;
+        int result = Max.max(a1, a2, a3);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To3To4To3Then4() {
+        int b1 = 3;
+        int b2 = 3;
+        int b3 = 4;
+        int b4 = 3;
+        int result = Max.max(b1, b2, b3, b4);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
 }
